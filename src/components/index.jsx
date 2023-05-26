2d28f38f293775f68aeed7a7fcc22045d0f1d697
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-05-26 21:12:46 +0700

    add Hero section

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,9 @@
-import About from './About';
+import About from './About/About';
 import Contact from './Contact';
 import Footer from './Footer';
-import Hero from './Hero';
-import Portfolio from './Portfolio';
+import Hero from './Hero/Hero';
+import Portfolio from './Portfolio/Portfolio';
 import Skills from './Skills';
-import Navbar from './Navbar/Navbar'
+import Navbar from './Navbar/Navbar';
 
 export { About, Contact, Footer, Hero, Navbar, Portfolio, Skills };
