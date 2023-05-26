44e83df06344eb63ff5cc55d1880c67d3a9febcd
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-05-26 21:05:50 +0700

    add Navbar, Sidebar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+      <h1>Hero</h1>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,8 +2,8 @@ import About from './About';
 import Contact from './Contact';
 import Footer from './Footer';
 import Hero from './Hero';
-import Navbar from './Navbar';
 import Portfolio from './Portfolio';
 import Skills from './Skills';
+import Navbar from './Navbar/Navbar'
 
 export { About, Contact, Footer, Hero, Navbar, Portfolio, Skills };
