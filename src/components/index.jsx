a491627260d070f52cd7d7ff54ddbf1880630908
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-05-31 18:23:29 +0700

    add Skills section

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,7 +3,7 @@ import Contact from './Contact';
 import Footer from './Footer';
 import Hero from './Hero/Hero';
 import Portfolio from './Portfolio/Portfolio';
-import Skills from './Skills';
+import Skills from './Skills/Skills';
 import Navbar from './Navbar/Navbar';
 
 export { About, Contact, Footer, Hero, Navbar, Portfolio, Skills };
