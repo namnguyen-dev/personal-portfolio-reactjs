96f4995f14c0711ee60765319377f3e3e77c0b14
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-05-31 18:50:15 +0700

    add Portfolio section

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    id: 1,
+    img: 'https://media.giphy.com/media/1YHfCEw7pDVgwDBmTY/giphy.gif',
+    title: 'Reverie E-commerce',
+    description: `An E-commerce website to sell luxury watches. This project was built using React, Airtable, Auth0, Stripe with features such as product listing, product details, shopping cart, user authentication with Auth0 and checkout with Stripe payment.`,
+    tags: ['React', 'Airtable', 'Auth0', 'Stripe', 'Tailwind'],
+    liveLink: 'https://watch-commerce-airtable-experiment.netlify.app/',
+    ghLink: 'https://github.com/namnguyen-dev/reverie-watch-commerce-reactjs',
+  },
+
+  {
+    id: 2,
+    img: 'https://media.giphy.com/media/pNtoZaNVF0VtpKEbRX/giphy.gif',
+    title: 'Vietnam Travel Website',
+    description: `A travel website where visitors can get detailed information, latest news & useful tips on visas, destinations & what to do in Vietnam. Developed using the ReactJS and styled using TailwindCSS.`,
+    tags: ['React', 'Tailwind'],
+    liveLink: 'https://vietnam-travel.netlify.app/',
+    ghLink: 'https://github.com/namnguyen-dev/vietnam-travel-website-reactjs',
+  },
+  {
+    id: 3,
+    img: 'https://media.giphy.com/media/t4Qz9GPq6uMDKRYyJ9/giphy.gif',
+    title: 'Portfolio Website',
+    description: `This project is the very first project I built on my own after breaking out of the tutorial hell. I got inspired by many great developers I have learned from Youtube, Udemy & Freecodecamp.`,
+    tags: ['React', 'CSS'],
+    liveLink: 'https://namdeveloper.netlify.app/',
+    ghLink: 'https://github.com/namnguyen-dev/personal-portfolio-reactjs',
+  },
+  {
+    id: 4,
+    img: 'https://media.giphy.com/media/7CGrdjhDs72sBOQk7A/giphy.gif',
+    title: 'Personal Blog',
+    description: `This is a personal blog project I built with Next.js 13, Airtable API. It features a minimalist design, responsive layout, markdown support.`,
+    tags: ['Next', 'Airtable', 'Tailwind'],
+    liveLink: 'https://namnguyen.vercel.app/',
+    ghLink: 'https://github.com/namnguyen-dev/personal-blog-nextjs',
+  },
+];
